f9456361e4b7d071cfdc6f45b204078ad28007e9
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2019-11-15 15:32:24 +0000

    updated libraries (#49)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,12 +4,11 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"   %% "bootstrap-play-26"   % "0.42.0",
+    "uk.gov.hmrc"   %% "bootstrap-play-26"   % "1.1.0",
     "uk.gov.hmrc"   %% "stub-data-generator" % "0.5.3"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-play-26"  % "0.42.0" % Test classifier "tests",
     "org.scalatest"          %% "scalatest"          % "3.0.8"  % "test",
     "com.typesafe.play"      %% "play-test"          % current  % "test",
     "org.pegdown"            % "pegdown"             % "1.6.0"  % "test, it",
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -10,10 +10,10 @@ addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "1.16.0")
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "1.19.0")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-artifactory" % "0.19.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-artifactory" % "0.20.0")
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "1.6.0")
 
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.6.20")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.6.23")
 
 addSbtPlugin("com.lucidchart" % "sbt-scalafmt" % "1.16")
